f332c7530c7c415964cf5ed33f83d5a923aa4fe1
Berk Yazicioglu <Brkk@w134-87-144-063.wireless.uvic.ca>
2014-10-15 13:27:57 -0700

    working code

---
+++ src/main/java/com/uvic/textshare/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package com.uvic.textshare;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HelloWorldServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+      resp.setContentType("text/plain");
+      resp.getWriter().println("Hello, fuck yeah AppEngine!");
+  }
+
+}
---
+++ src/main/java/com/uvic/textshare/service/model/Textbook.java
@@ -0,0 +1,105 @@
+package com.uvic.textshare.service.model;
+
+import java.util.Date;
+
+import com.google.appengine.api.users.User;
+
+public class Textbook {
+	private String title;
+    private String author;
+    private Date addDate;
+    private User user;
+    private String isbn;
+    private String condition;
+    private String edition;//edition or edition?
+    private String type;
+    private String matched;
+    
+    //need to add an ID field, maybe randomly generate it?
+    public Textbook(String title, String author, Date addDate, User user,
+			String isbn, String condition, String edition, String type, String matched) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.addDate = addDate;
+		this.user = user;
+		this.isbn = isbn;
+		this.condition = condition;
+		this.edition = edition;
+		this.type = type;
+		this.matched = matched;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getAddDate() {
+		return addDate;
+	}
+
+	public void setAddDate(Date addDate) {
+		this.addDate = addDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public String getMatched() {
+		return matched;
+	}
+
+	public void setMatched(String matched) {
+		this.matched = matched;
+	}
+
+}
