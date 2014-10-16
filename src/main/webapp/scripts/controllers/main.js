e238f25d7ddc11d6cb0afa724dc6ebafcdbd4861
James Hedin <j.hedin10@gmail.com>
2014-10-16 02:05:53 -0700

    scrapping the grunt output, and using the originals

---
+++ src/main/webapp/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name textShareApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the textShareApp
+
+angular.module('textShareApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+*/
