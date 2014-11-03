295c4e391c6c6cab6d456b99dcb31d2ff503921a
Logan <logan.masniuk@gmail.com>
2014-11-03 14:54:54 -0800

    Refactoring the front end

---
+++ src/main/webapp/scripts/controllers/typeAheadCtrl.js
@@ -0,0 +1,25 @@
+
+app.controller('typeAheadCtrl', function($scope, $http, user) {
+      // $scope.selected = '';
+      
+          $scope.updateList = function(title){
+              return $http({
+                method: 'GET',
+                url: 'https://www.googleapis.com/books/v1/volumes?q='+title+'&maxResults=5'
+              }).then(function ($response)
+              {
+                // var result = [];
+                // $scope.booksList = $response.data['items'];
+                // angular.forEach($scope.booksList, function(obj){
+                      // result.push(obj['volumeInfo']['title']);
+                // })
+                return $response.data['items'];
+              });
+        }
+
+          $scope.onSelect = function (item) {
+              console.log($scope.asyncSelected);
+              user.activeBookProperties.author = $scope.asyncSelected['volumeInfo']['authors'][0];
+              user.activeBookProperties.isbn = $scope.asyncSelected['volumeInfo']['industryIdentifiers'][0]['identifier'];
+          };
+});
