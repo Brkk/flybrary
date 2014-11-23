d53e2eadce0e2aa81967bd3541b2c176edc12da3
Jordan Lerner <jordan.m.lerner@gmail.com>
2014-11-22 18:52:30 -0800

    Updated the matching function and mail handler servlet to have pretty emails and removed the <MATCH_DATE> at the bottom of emails

--- src/main/java/com/uvic/textshare/service/rest/MailHandlerServlet.java
+++ src/main/java/com/uvic/textshare/service/rest/MailHandlerServlet.java
@@ -2,7 +2,6 @@ package com.uvic.textshare.service.rest;
 
 import java.io.IOException;
 import java.util.Properties;
-
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
@@ -11,42 +10,81 @@ import javax.servlet.http.*;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import java.util.Date;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import com.uvic.textshare.service.matching.MatchingFunction;
+import com.uvic.textshare.service.model.*;
+import com.google.appengine.api.users.*;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-
+import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
-@SuppressWarnings("serial")
 public class MailHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    String noMatchMsgBody = "Hello student,\n"
-        + "Unfortunatley the match that you are replying to has been disconnected and you are no longer able to reach your match."
-        + "Fortunatley we have already started to look for an another match for your textbook! We will get back to you, "
-        + "when we find one."
-        + "Have a fantastic day and remember to always fly with flybrary.\n\n"
-        + "Cheers,\n"
-        + "Team Flybrary\n\n<MATCH_DATE>";
+    String emailBody = "";
+    String fromEmailAddress = "";
     try {
+      String noMatchMsgBody = "Hello student,\n"
+          + "Unfortunatley the match that you are replying to has been disconnected and you are no longer able to reach your match."
+          + "Fortunatley you are able to go back to www.Flybrary.ca to try and find another match for your textbook!"
+          + "Have a fantastic day and remember to always fly with flybrary.\n\n"
+          + "Regards,\n"
+          + "Kisses from Team Flybrary\n\n";
+
       Properties props = new Properties();
       Session session = Session.getDefaultInstance(props, null);
       MimeMessage message = new MimeMessage(session, req.getInputStream());
 
       MimeMultipart content=(MimeMultipart)message.getContent();
 
-      String emailBody = content.getBodyPart(0).getContent().toString();
-      String[] matchDateTokens = emailBody.split("<MATCH_DATE>");
-      String emailMatchedDate = matchDateTokens[1];
-      String fromEmailAddress = ((InternetAddress)message.getFrom()[0]).getAddress().toString();
+      emailBody = content.getBodyPart(0).getContent().toString();
+      fromEmailAddress = ((InternetAddress)message.getFrom()[0]).getAddress().toString();
+      String emailSubject = message.getSubject();
+      String messageID = message.getMessageID();
+
+
+      Pattern pattern = Pattern.compile("\\d+/\\d+/\\d+ +\\d+:\\d+:\\d+");
+      Matcher matcher = pattern.matcher(emailBody);
+      String emailMatchedDate = "";
+      while (matcher.find()) {
+        int i = 0;
+        emailMatchedDate = matcher.group(i);
+        i++;
+      }
+
+      System.out.println("emailMatchedDate: " + emailMatchedDate);
+      System.out.println("emailBody: " + emailBody);
+      System.out.println("fromEmailAddress: " + fromEmailAddress);
+      System.out.println("messageID: " + messageID);
+      System.out.println("emailSubject: " + emailSubject);
 
       Filter matchFilter = new FilterPredicate("matchDate", FilterOperator.EQUAL, emailMatchedDate);
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
@@ -54,21 +92,25 @@ public class MailHandlerServlet extends HttpServlet {
       Entity match = datastore.prepare(q).asSingleEntity();
 
       if(match == null) {
-        sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", fromEmailAddress, "Match Does not Exist", noMatchMsgBody);
+        sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", fromEmailAddress, "Match Does not Exist", noMatchMsgBody+"\n\n Match Date:"+emailMatchedDate);
       } else {
         String firstUsersEmail = match.getProperty("firstUsersEmail").toString();
         String secondUsersEmail = match.getProperty("secondUsersEmail").toString();
 
+        System.out.println("firstUsersEmail: " + firstUsersEmail);
+        System.out.println("secondUsersEmail: " + secondUsersEmail);
+
         if(fromEmailAddress.equals(secondUsersEmail)) {
-          sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", firstUsersEmail, "", emailBody);
+          sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", firstUsersEmail, emailSubject, emailBody);
         } else if (fromEmailAddress.equals(firstUsersEmail)) {
-          sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", secondUsersEmail, "", emailBody);
+          sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", secondUsersEmail, emailSubject, emailBody);
         } else {
-          sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", fromEmailAddress, "Match Does not Exist", noMatchMsgBody);
+          sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", fromEmailAddress, "Match Does not Exist", noMatchMsgBody+"\n\n Match Date:"+emailMatchedDate);
         }
       }
     } catch (Exception e) {
-        sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", "team.textshare@gmail.com", "Email from User", noMatchMsgBody);
+        sendEmail("email@textchngr.appspotmail.com", "Team Flybrary", "team.flybrary@gmail.com", "Error Email from User", emailBody + "\n\nThis email was sent as a result from an error from user" + fromEmailAddress);
+        System.out.println(e.toString());
         e.printStackTrace();
     }
   }
@@ -76,6 +118,7 @@ public class MailHandlerServlet extends HttpServlet {
   public void sendEmail(String fromEmailAddress, String fromUserName, String toEmailAddress, String emailSubject, String emailBody) {
     Properties props = new Properties();
     Session session = Session.getDefaultInstance(props, null);
+    System.out.println("toEmailAddress: " + toEmailAddress);
     try {
       Message msg = new MimeMessage(session);
       try {
@@ -90,7 +133,8 @@ public class MailHandlerServlet extends HttpServlet {
         e.printStackTrace(); //log these errors
       }
       msg.setSubject(emailSubject);
-      msg.setText(emailBody);
+      //msg.setText(emailBody);
+      msg.setContent(emailBody, "text/html; charset=utf-8");
       Transport.send(msg);
     } catch (AddressException e) {
         e.printStackTrace();
