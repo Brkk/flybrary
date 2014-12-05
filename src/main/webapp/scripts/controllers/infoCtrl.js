3be2db9c0c032ba9cc51dbe51656a12b392d05a2
Logan <logan.masniuk@gmail.com>
2014-12-04 19:25:07 -0800

    minor fixes

--- src/main/webapp/scripts/controllers/infoCtrl.js
+++ src/main/webapp/scripts/controllers/infoCtrl.js
@@ -1,5 +1,17 @@
-app.controller('infoController', function(){
+app.controller('infoController', function($scope){
 	this.section = 0;
+
+	$scope.sections = {
+    "sxns": [ 
+    	"Offering a Book",
+    	 "Requesting a Book", 
+    	 "Viewing your Matches", 
+    	 "Re-matching",
+    	 "Cofirming your Matches",
+    	 "Updating your Location and Search Radius",
+    	 "Contact Us"
+    	 ] 
+  };
 	
 	this.selectSection = function(setSection) {
       this.section = setSection;
